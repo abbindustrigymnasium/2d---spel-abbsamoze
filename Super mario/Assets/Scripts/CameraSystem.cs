d4df8537c8bddea0d5402bc30c5feea1920b068c
abbsamoze <69853489+abbsamoze@users.noreply.github.com>
2021-03-30 16:04:13 +0200

    Made camera follow Mario

--- Super mario/Assets/Scripts/CameraSystem.cs
+++ Super mario/Assets/Scripts/CameraSystem.cs
@@ -5,20 +5,17 @@ using UnityEngine;
 public class CameraSystem : MonoBehaviour
 {
 
-    public GameObject player;
-    public float xMin;
-    public float xMax;
-    public float yMin;
-    public float yMax;
+    public Transform target;
+
     // Start is called before the first frame update
     void Start () {
-        player = GameObject.FindGameObjectWithTag("Player");
     
     }
     // Update is called once per frame  
-    void LateUpdate(){
-        float x = Mathf.Clamp (player.transform.position.x, xMin, xMax);
-        float y = Mathf.Clamp (player.transform.position.y, yMin, yMax);
-        gameObject.transform.position = new Vector3(x, y, gameObject.transform.position.z);
-    }
+  void LateUpdate () {
+    if(target == null) return;
+    transform.position = new Vector3(target.position.x,
+                                     transform.position.y,
+                                     transform.position.z);
+}
 }
